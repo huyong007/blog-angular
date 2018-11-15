665f93ce2cb3da270e24120ba8f3dea32dd91d0f
胡永 <huy@caiyoudata.com>
2018-11-15 21:34:57 +0800

    新建列表页，开始使用angular

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET posts listing. */
+let postsList = ['文章1', '文章2', '文章3']
 router.get('/', function(req, res, next) {
-  res.render('posts', { title: '创作我的创作' });
+  res.render('posts', { title: '创作我的创作',postsList });
 });
   
 module.exports = router;
