2e598e2025f25484b9a528d81dda803419f52a4d
胡永 <huy@caiyoudata.com>
2018-11-15 23:32:50 +0800

    拿到数据并进行渲染

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'myBlog',content:'8:模版引擎' });
+  res.render('index', { title: 'myBlog',content:'12:路由归类' });
 });
 
 module.exports = router;
