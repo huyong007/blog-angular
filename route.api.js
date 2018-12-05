c0b338ce7e000407a910027251df7ded91229bf1
胡永 <huy@caiyoudata.com>
2018-12-05 23:21:59 +0800

    新增页面

--- route.api.js
+++ route.api.js
@@ -9,7 +9,7 @@ var PostModel = require('./models/post');
 //   res.json({postsList: ['文章1', '文章2', '文章3']});
 // });
 
-router.get('/list', function(req, res, next) {
+router.get('/list', function (req, res, next) {
   PostModel.find({}, {}, function (err, posts) {
     if (err) {
       res.json({ success: false });
@@ -19,11 +19,11 @@ router.get('/list', function(req, res, next) {
   });
 });
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
-    res.send('respond with a resource');
-  });
+router.get('/users', function (req, res, next) {
+  res.send('respond with a resource');
+});
 
-  /* Creat creat */
+/* Creat creat */
 router.post('/create', function (req, res, next) {
   var title = req.body.title;
   var content = req.body.content;
@@ -31,7 +31,34 @@ router.post('/create', function (req, res, next) {
   post.title = title;
   post.content = content;
   post.save(function (err, doc) {
-    res.json({success: true});
+    res.json({ success: true });
+  });
+});
+/* GET one post */
+router.get('/posts/one', function (req, res, next) {
+  var id = req.query.id;
+
+  PostModel.findOne({ _id: id }, function (err, post) {
+    if (err) {
+      res.json({ success: false });
+      return;
+    }
+
+    res.json({ success: true, post });
+  });
+});
+/* PATCH edit post */
+router.post('/posts/edit', function (req, res, next) {
+  var id = req.body.id;
+  var title = req.body.title;
+  var content = req.body.content;
+
+  PostModel.findOneAndUpdate({ _id: id }, { title, content }, function (err) {
+      if (err) {
+          res.json({ success: false });
+      } else {
+          res.json({ success: true });
+      }
   });
 });
 module.exports = router;
--- route.page.js
+++ route.page.js
@@ -32,4 +32,13 @@ router.get('/posts/show', function (req, res, next) {
         res.render('show', { post });
     });
 });
+
+/* GET posts edit page. */
+router.get('/posts/edit', function (req, res, next) {
+    var id = req.query.id;
+
+    res.render('edit', { id });
+});
+
+
 module.exports = router;
