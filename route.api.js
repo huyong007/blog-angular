949ae896c5c6770ad029050794ad882bd9e82c38
胡永 <huy@caiyoudata.com>
2018-12-13 13:38:59 +0800

    api添加版本

--- route.api.js
+++ route.api.js
@@ -4,7 +4,7 @@ var ArticalModel = require('./models/artical');
 var PostModel = require('./models/post');
 
 /* GET posts listing. */
-router.get('/list', function (req, res, next) {
+router.get('/v1/list', function (req, res, next) {
   PostModel.find({}, {}, function (err, posts) {
     if (err) {
       res.json({ success: false });
@@ -14,12 +14,12 @@ router.get('/list', function (req, res, next) {
   });
 });
 /* GET users listing. */
-router.get('/users', function (req, res, next) {
+router.get('/v1/users', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 /* Creat creat */
-router.post('/create', function (req, res, next) {
+router.post('/v1/create', function (req, res, next) {
   var title = req.body.title;
   var content = req.body.content;
   var post = new PostModel();
@@ -30,7 +30,7 @@ router.post('/create', function (req, res, next) {
   });
 });
 /* GET one post */
-router.get('/posts/one', function (req, res, next) {
+router.get('/v1/posts/one', function (req, res, next) {
   var id = req.query.id;
 
   PostModel.findOne({ _id: id }, function (err, post) {
@@ -43,7 +43,7 @@ router.get('/posts/one', function (req, res, next) {
   });
 });
 /* PATCH edit post */
-router.post('/posts/edit', function (req, res, next) {
+router.post('/v1/posts/edit', function (req, res, next) {
   var id = req.body.id;
   var title = req.body.title;
   var content = req.body.content;
